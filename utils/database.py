2bccd921f9d7566baa1dbfd3df273bbbea245d62
John Doe <johndoe@example.com>
2025-03-03 14:10:57 +0100

    030325 dziala na dockerze

--- utils/database.py
+++ utils/database.py
@@ -1,5 +1,4 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-import gridfs
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorGridFSBucket
 import os
 
 # Pobranie URI MongoDB z pliku .env lub użycie domyślnego
@@ -10,10 +9,16 @@ DATABASE_NAME = "file_storage"
 client = AsyncIOMotorClient(MONGO_URI)
 db = client[DATABASE_NAME]
 
-# Tworzymy GridFS do przechowywania plików w bazie
-fs = gridfs.GridFS(db)
+# Używamy asynchronicznego GridFS
+fs = AsyncIOMotorGridFSBucket(db)
 
 async def save_file_to_db(filename: str, content: bytes, content_type: str):
     """Zapisuje plik do MongoDB GridFS."""
-    file_id = fs.put(content, filename=filename, content_type=content_type)
-    return file_id
+    from motor.motor_asyncio import AsyncIOMotorGridIn
+
+    # Otwieramy nowy plik do zapisu
+    grid_in = await fs.open_upload_stream(filename, metadata={"content_type": content_type})
+    await grid_in.write(content)
+    await grid_in.close()
+
+    return grid_in._id  # Zwraca ID zapisanego pliku
