bfb001a3b685db27e1d4b61b7b95af585bfdd0e3
John Doe <johndoe@example.com>
2025-03-13 17:51:02 +0100

    proba pzred uploadem do azure

---
+++ vue-project/src/components/BrowserComponent.vue
@@ -0,0 +1,29 @@
+ <template>
+    <div>
+      <input type="text" v-model="currentUrl" @keyup.enter="updateUrl" placeholder="Enter URL" />
+      <iframe :src="currentUrl" width="100%" height="500px" style="border: none;"></iframe>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'BrowserComponent',
+    props: {
+      url: {
+        type: String,
+        default: '',
+      },
+    },
+    data() {
+      return {
+        currentUrl: this.url,
+      };
+    },
+    methods: {
+      updateUrl() {
+        this.currentUrl = this.currentUrl;
+      },
+    },
+  };
+  </script>
+  
